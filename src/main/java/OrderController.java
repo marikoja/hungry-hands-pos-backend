f5ceee1651fe6a4fe159e63e4842673576ad929a
marikoja <Marikoanderson@gmail.com>
2018-07-17 19:29:34 -0700

    post request order items

--- src/main/java/OrderController.java
+++ src/main/java/OrderController.java
@@ -9,15 +9,27 @@ public class OrderController {
 
     public OrderController(Connection conn) {
 
-        get("/hello", (request, response) -> {
-            return "Hello World";
-        });
-
         post("/order", (req, res) -> {
 
             JSONObject obj = new  JSONObject(req.body());
 
             String SQL = "INSERT INTO \"order\" (customer_id, status, company_id) VALUES ("+obj.get("customer_id")+",'"+obj.get("status")+"',1) RETURNING order_id";
+            int count = 0;
+            String results = null;
+
+            try (Statement stmt = conn.createStatement();ResultSet rs = stmt.executeQuery(SQL)) {
+                results = JsonUtil.convertResultSetIntoJSON(rs).toString();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            return results;
+        });
+
+        post("/order/:order_id", (req, res) -> {
+
+            JSONObject obj = new  JSONObject(req.body());
+
+            String SQL = "INSERT INTO order_menu_item (menu_item_id, quantity) VALUES ("+obj.get("menu_item_id")+",1) RETURNING order_menu_item_id";
             int count = 0;
             String results = null;
 
