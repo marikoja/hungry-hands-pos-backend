5f9704b902c54a301a55119f59b5b6fae91e236a
marikoja <Marikoanderson@gmail.com>
2018-07-17 19:53:21 -0700

    refactored post request

--- src/main/java/OrderController.java
+++ src/main/java/OrderController.java
@@ -29,7 +29,7 @@ public class OrderController {
 
             JSONObject obj = new  JSONObject(req.body());
 
-            String SQL = "INSERT INTO order_menu_item (menu_item_id, quantity) VALUES ("+obj.get("menu_item_id")+",1) RETURNING order_menu_item_id";
+            String SQL = "INSERT INTO order_menu_item (menu_item_id, quantity, order_id) VALUES ("+obj.get("menu_item_id")+",1,"+ req.params(":order_id")+") RETURNING order_menu_item_id, order_id";
             int count = 0;
             String results = null;
 
