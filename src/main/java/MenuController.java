93bed4ca81a4e8b6e104a3f6790517765b17e2ef
marikoja <Marikoanderson@gmail.com>
2018-07-19 01:04:22 -0700

    put response for edit item

--- src/main/java/MenuController.java
+++ src/main/java/MenuController.java
@@ -56,5 +56,26 @@ public class MenuController {
             }
             return results;
         });
+
+        put("/menu/:menu_id/menu_item/:menu_item_id", (req, res) -> {
+
+            JSONObject obj = new  JSONObject(req.body());
+
+            String SQL = "UPDATE menu_item " +
+                    "SET (name, price, description, quantity, img) " +
+                    "= ('"+obj.get("name")+"',"+obj.get("price")+",'"+obj.get("description")+"',"+obj.get("quantity")+",'"+obj.get("img")+"') " +
+                    "WHERE menu_id = "+req.params(":menu_id")+" AND menu_item_id = "+req.params(":menu_item_id")+" " +
+                    "RETURNING menu_item_id, name;";
+
+            int count = 0;
+            String results = null;
+
+            try (Statement stmt = conn.createStatement();ResultSet rs = stmt.executeQuery(SQL)) {
+                results = JsonUtil.convertResultSetIntoJSON(rs).toString();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            return results;
+        });
     }
 }
